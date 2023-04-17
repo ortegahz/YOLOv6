6e043354ea281b3370449b6b46ca6e1e30972200
manu <zxthz@126.com>
2023-04-17 18:06:00 +0800

    mpa_s480_e10w5_nc_ff_nab_ia - ap0.405

--- yolov6/utils/checkpoint.py
+++ yolov6/utils/checkpoint.py
@@ -13,6 +13,11 @@ def load_state_dict(weights, model, map_location=None):
     ckpt = torch.load(weights, map_location=map_location)
     state_dict = ckpt['model'].float().state_dict()
     model_state_dict = model.state_dict()
+    # init new branch ws by orig ws
+    for k, v in model_state_dict.items():
+        if 'face' in k:
+            kc = k.replace('face_', '')
+            state_dict[k] = state_dict[kc]
     state_dict = {k: v for k, v in state_dict.items() if k in model_state_dict and v.shape == model_state_dict[k].shape}
     model.load_state_dict(state_dict, strict=False)
     del ckpt, state_dict, model_state_dict

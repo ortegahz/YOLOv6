073bbce97911d67880c8e66c11c25467765087d4
manu <zxthz@126.com>
2023-04-18 11:55:58 +0800

    mpa_s480_e10w5_nc_ff_full_ia - ap0.394

--- yolov6/core/engine.py
+++ yolov6/core/engine.py
@@ -159,9 +159,9 @@ class Trainer:
             elif self.args.fuse_ab:
                 if state == 'face':
                     total_loss, loss_items = self.compute_loss((preds[0],preds[-2],preds[-1]), targets, epoch_num, step_num)
-                    # total_loss_ab, loss_items_ab = self.compute_loss_ab((preds[0],preds[3],preds[4]), targets, epoch_num, step_num)
-                    # total_loss += total_loss_ab
-                    # loss_items += loss_items_ab
+                    total_loss_ab, loss_items_ab = self.compute_loss_ab((preds[0],preds[3],preds[4]), targets, epoch_num, step_num)
+                    total_loss += total_loss_ab
+                    loss_items += loss_items_ab
                 else:
                     total_loss, loss_items = self.compute_loss((preds[0],preds[-4],preds[-3]), targets, epoch_num, step_num)
                     total_loss_ab, loss_items_ab = self.compute_loss_ab((preds[0],preds[1],preds[2]), targets, epoch_num, step_num)

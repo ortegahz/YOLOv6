8fbf5a9abd82d350482499ebbeb0734af80af40a
manu <zxthz@126.com>
2023-07-12 17:00:23 +0800

    export onnx for nnie

---
+++ yolov6/models/nnie.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+
+class Nnie(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+
+    def forward(self, im):
+        y = self.model(im)
+        y = y[0] if isinstance(y, list) else y
+        return y
